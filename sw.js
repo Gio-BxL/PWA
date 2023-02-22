a456aea3fa60620b30fddd353aa0bb2f694bdb87
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2023-02-21 18:39:47 -0600

    modify the sw file

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
 const STATIC_CACHE = "static";
-const APP_SHELL =[
+const APP_SHELL = [
     "/",
     "/index.html",
-    "/styles/style.css",
-    "/js/functions.js",
-    "/js/main.js",
-    "/js/script1.js",
-    "/js/script2.js",
-    "/imagenes/iconos/512.png",
-    "/js/notificaciones.js",
-    "imagenes/pwalogo.png",
+    "/style.css",
+    "/main.js",
+    "/Assets.xcassets/512.png",
+    "imagenes/plane.png",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",
 
-] ;
-self.addEventListener("install",(e) => {
+];
+self.addEventListener("install", (e) => {
     console.log("entrando a instalar");
     const cacheStatic = caches
-    .open(STATIC_CACHE)
+        .open(STATIC_CACHE)
 });
 
 self.addEventListener("fetch", (e) => {
